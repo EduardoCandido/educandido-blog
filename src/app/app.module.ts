00d2ba9db376aa796b9227e04ce5d6f115a42523
Eduardo Candido <eduardocandidopro@gmail.com>
2021-02-09 21:06:56 -0300

    Criado novo componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,14 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from "@angular/router";
-import { AppComponent } from "./app.component";
-
+import { HomeComponent } from "./components/home/home.component";
 export const ROUTES: Routes = [
-    {path: '', component: AppComponent}
+    {path: '', component: HomeComponent}
 ]
